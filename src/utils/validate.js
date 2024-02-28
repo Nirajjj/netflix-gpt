364dd1f33327b4153fa6ea1d08f16ba715c80617
Niraj Parab <nirajparab007@gmail.com>
2024-02-28 13:21:46 +0530

    update

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -3,9 +3,7 @@ const validOrNot = (email, password) => {
   const isEmailValid = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
 
   const isPassValid =
-    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?~`]).{8,}$/.test(
-      password
-    );
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
   // /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,}$/.test(
 
   if (!isEmailValid) return "email not valid";
